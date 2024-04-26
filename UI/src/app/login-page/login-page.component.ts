3d6d96321db966072589de876757eed55a70ecdd
parashgupta169 <155993499+parashgupta169@users.noreply.github.com>
2024-04-26 18:44:53 +0530

    comment remove from login-ts

--- UI/src/app/login-page/login-page.component.ts
+++ UI/src/app/login-page/login-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-//import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-page',
@@ -10,29 +9,17 @@ import { Router } from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   constructor(private router: Router) { }
-  // constructor(private router: Router, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   onLogin(username: string, password: string): void {
     if (!username || !password) {
-      //this.showSnackbar('Please enter both username and password', 'error-snackbar');
     } else if (username === 'parash' && password === '123') {
       this.router.navigate(['/home']);
       console.log('Login successful!');
     } else {
       console.log('Invalid username or password');
-      //this.showSnackbar('Invalid username or password', 'error-snackbar');
     }
   }
-
-
-  // private showSnackbar(message: string, panelClass: string): void {
-  //   const config = new MatSnackBarConfig();
-  //   config.duration = 5000;
-  //   config.panelClass = [panelClass];
-  //   config.verticalPosition = 'top';
-  //   this.snackBar.open(message, 'Close', config);
-  // }
 }
